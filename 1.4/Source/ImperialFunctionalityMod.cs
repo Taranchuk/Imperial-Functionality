37bec95be5be9891824f02a30b91529fedd54600
Taranchuk <nfhfyxer@gmail.com>
2023-10-15 03:22:04 +0300

    update

---
+++ 1.4/Source/ImperialFunctionalityMod.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace ImperialFunctionality
+{
+    public class ImperialFunctionalityMod : Mod
+    {
+        public ImperialFunctionalityMod(ModContentPack pack) : base(pack)
+        {
+			new Harmony("ImperialFunctionalityMod").PatchAll();
+        }
+    }
+}
