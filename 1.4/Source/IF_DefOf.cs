e087f559af75716618a832c8e40a3b805ec0f587
Taranchuk <nfhfyxer@gmail.com>
2023-10-18 11:04:41 +0300

    techprint task

---
+++ 1.4/Source/IF_DefOf.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ImperialFunctionality
+{
+    [DefOf]
+    public static class IF_DefOf
+    {
+        public static ThingDef Plant_Psychoid, Plant_Cotton, Plant_Tinctoria, Plant_Healroot, VFED_WaterMain;
+    }
+}
