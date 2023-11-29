ce936f5d44c1eefaf17b5e9f8b220ba44eea7849
Taranchuk <nfhfyxer@gmail.com>
2023-11-29 07:41:12 +0200

    prevent survelliance visibility trigger + remove free intel on player built buildings

--- 1.4/Source/IncidentWorker_Raid_TryResolveRaidFaction_Patch.cs
+++ 1.4/Source/IncidentWorker_Raid_TryResolveRaidFaction_Patch.cs
@@ -15,18 +15,15 @@ namespace ImperialFunctionality
             if (__result && parms.target is Map map)
             {
                 var stations = map.listerBuildings.AllBuildingsColonistOfDef(IF_DefOf.VFED_SurveillanceStation).ToList();
-                Log.Message("stations: " + string.Join(", ", stations));
                 foreach (var station in stations)
                 {
                     var comp = station.GetComp<CompSurveillanceScanner>();
                     var pointsToReduce = Mathf.Min(comp.raidPointsFound, parms.points);
                     parms.points -= pointsToReduce;
-                    Log.Message("reducing points by " + pointsToReduce);
                     comp.raidPointsFound -= pointsToReduce;
                     if (parms.points <= 0)
                     {
                         __result = false;
-                        Log.Message("cancelling raid ");
                     }
                 }
             }
