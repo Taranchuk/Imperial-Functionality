35922853e0b5007390e9d62c1ba4db968284c918
Taranchuk <nfhfyxer@gmail.com>
2023-12-12 19:28:59 +0200

    added Royal Invitation + title abilities fix

--- 1.4/Source/IF_DefOf.cs
+++ 1.4/Source/IF_DefOf.cs
@@ -11,5 +11,8 @@ namespace ImperialFunctionality
         public static FactionDef VFEE_Deserters;
         public static ThingDef VFED_SurveillancePillar, VFED_SurveillanceStation;
         public static ThingDef IF_ImperialPropaganda;
+        public static QuestScriptDef VFEE_GrandBall, VFEE_ArtExhibit;
+        public static AbilityDef IF_FormEmpireAlliance, IF_RoyalInvitation;
+        public static RoyalTitleDef VFEE_Despot, VFEE_Archduke;
     }
 }
